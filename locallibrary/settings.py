7bd79b96ba4edb80fe83a7cf378892035d3cd295
Dow Drake <dowdrake@gmail.com>
2023-01-23 10:51:58 -0800

    try removing secure session cookie

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # In production assume https enabled for whole site
 # SECURE_HSTS_SECONDS = 60 if PRODUCTION else 0
 # SECURE_SSL_REDIRECT = PRODUCTION
-SESSION_COOKIE_SECURE = PRODUCTION
+# SESSION_COOKIE_SECURE = PRODUCTION
 CSRF_COOKIE_SECURE = PRODUCTION
 CSRF_TRUSTED_ORIGINS = ['https://web-production-78d2.up.railway.app']
 
