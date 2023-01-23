3d2929e7e16c5a959c4b6f16f484dc30811af9d2
Dow Drake <dowdrake@gmail.com>
2023-01-23 09:37:56 -0800

    try logging to console

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -158,3 +158,17 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
